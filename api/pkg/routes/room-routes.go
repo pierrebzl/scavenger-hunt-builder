e4b1cca6b825906477d5ea489ff18793923054d8
Pierre Bonzel <pierre.bonzel@gmail.com>
2020-09-20 01:27:00 -0400

    Update room routes

--- api/pkg/routes/room-routes.go
+++ api/pkg/routes/room-routes.go
@@ -5,10 +5,10 @@ import (
 	"github.com/gorilla/mux"
 )
 
-var RegisterRoomRoutes = func(router *mux.Router) {
+var RoomRoutes = func(router *mux.Router) {
 	router.HandleFunc("/room/", controllers.CreateRoom).Methods("POST")
 	router.HandleFunc("/room/", controllers.GetRoom).Methods("GET")
-	router.HandleFunc("/room/{id:[0-9]+}", controllers.GetRoomById).Methods("GET")
-	router.HandleFunc("/room/{id:[0-9]+}", controllers.UpdateRoom).Methods("PUT")
-	router.HandleFunc("/room/{id:[0-9]+}", controllers.DeleteRoom).Methods("DELETE")
+	router.HandleFunc("/room/{roomId:[0-9]+}", controllers.GetRoomById).Methods("GET")
+	router.HandleFunc("/room/{roomId:[0-9]+}", controllers.UpdateRoom).Methods("PUT")
+	router.HandleFunc("/room/{roomId:[0-9]+}", controllers.DeleteRoom).Methods("DELETE")
 }
